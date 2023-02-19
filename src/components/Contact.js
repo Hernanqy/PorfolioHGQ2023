6446f8c3bd04e74ce18f4fa6f9719351cd5b3731
hernan gustavo quiroga <desarrolladoryotti@gmail.com>
2023-02-19 20:55:52 -0300

    listo para refactorizacion

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React from "react";
+
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../variants";
 
 const Contact = () => {
-  return <div className='section' id='contact'>Contact</div>;
+  return (
+    <section className="lg:section py-16" id="contact">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          <motion.div
+          variants={fadeIn('right',0.3)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once: false, amount: 0.3}}
+          className="flex-1 flex justify-start items-center">
+            <div>
+              <h4 className="text-xl uppercase text-cyan-600 font-medium mb-2 tracking-wide">
+                Contacto
+              </h4>
+              <h2 className="text-[45px] lg:text-[90px] leading-none mb-12">
+                Trabajemos <br /> Juntos
+              </h2>
+            </div>
+          </motion.div>
+
+          <motion.form
+          variants={fadeIn('left',0.3)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once: false, amount: 0.3}}
+          className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start">
+            <input
+              className="bg-transparent border-b py-3 out-line-none w-full placeholder:text-white focus:border-cyan-500 transition-all"
+              type="text"
+              placeholder="Nombre"
+            />
+            <input
+              className="bg-transparent border-b py-3 out-line-none w-full placeholder:text-white focus:border-cyan-500 transition-all"
+              type="text"
+              placeholder="Email"
+            />
+            <textarea className="bg-transparent border-b py-3 out-line-none w-full placeholder:text-white focus:border-cyan-500 transition-all resize-none mb-12" placeholder="Aqui el mensaje" ></textarea>
+            <button className="btn btn-lg">Enviar Mensaje</button>
+          </motion.form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/Work.js
+++ src/components/Work.js
@@ -13,7 +13,12 @@ const Work = () => {
     <section className="section" id="work">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row gap-x-10">
-          <div className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
+          <motion.div
+          variants={fadeIn('right',0.3)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once: false, amount: 0.3}}
+          className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-11">
             <div>
               <h2 className="h2 leading-tight text-cyan-600 ">
                 Mis ultimos
@@ -41,8 +46,13 @@ const Work = () => {
                 <span className="text-3xl text-white">Titulo del Proyecto</span>
               </div>
             </div>
-          </div>
-          <div className="flex-1 flex flex-col gap-y-10">
+          </motion.div>
+          <motion.div
+          variants={fadeIn('left',0.3)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once: false, amount: 0.3}}
+          className="flex-1 flex flex-col gap-y-10">
           <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
               <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all first-line:duration-300"></div>
               <img
@@ -73,7 +83,7 @@ const Work = () => {
                 <span className="text-3xl text-white">Titulo del Proyecto</span>
               </div>
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
