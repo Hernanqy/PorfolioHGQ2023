2440dbff038480037930889226ac4b761dea2958
hernan gustavo quiroga <desarrolladoryotti@gmail.com>
2023-05-06 14:55:08 -0300

    agregandoweb

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,10 +3,10 @@ import React from "react";
 import { motion } from "framer-motion";
 
 import { fadeIn } from "../variants";
-
+import "./Work.css"
 import Img1 from "../assets/portada_malon.png";
 import Img2 from "../assets/portadagracita.png";
-import Img3 from "../assets/portfolio-img3.png";
+import Img3 from "../assets/Tecnologia Educativa.jpg";
 
 const Work = () => {
   return (
@@ -26,9 +26,17 @@ const Work = () => {
                 trabajos
               </h2>
               <p className="max-w-sm mb-16">
-                lorem kdlkasldklkklckslck mcsocmo mocmsomc
-                kmcskmcksmckmskcmkcmksmcksmcksmckmskcm mcksmcksmckmskcmksmcksmc.
+                Trabajos realizados con las siguientes tecnologias:
+                         
               </p>
+              <ol>
+                <li>Html</li>
+                <li>Css</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+                <li>Javascript</li>
+              </ol>
+              
               
             </div>
             <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
@@ -72,15 +80,16 @@ const Work = () => {
               <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all first-line:duration-300"></div>
               <img
                 className="group-hover:scale-125 transition-all duration-500"
-                src={Img1}
+                src={Img3}
                 alt=""
               />
               <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50 ">
-                <span className="text-gradient">UX/UI Design</span>
+              <button className="btn btn-sm"><a href='https://educatec.vercel.app/'>Ver el proyecto</a></button>
+                <span className="text-gradient">Tecnologia Educativa</span>
                 
               </div>
               <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50 ">
-                <span className="text-3xl text-white">Titulo del Proyecto</span>
+                <span className="text-3xl text-white">React-Bootstrap</span>
               </div>
             </div>
           </motion.div>
